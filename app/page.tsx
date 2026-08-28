cfa561e1730d19c622a54bf518deb5e0faed46fc
Jeff Jackson <105036000+jeffRnR@users.noreply.github.com>
2026-08-28 09:59:44 +0300

    Fix grammar
    Corrected grammatical errors in the message.

--- app/page.tsx
+++ app/page.tsx
@@ -641,7 +641,7 @@ export default function Home() {
                 } mb-8`}
               >
                 You are loved on this side... in case of the rare occassion that you find yourself here please know that you are incredibly missed from 
-                miles away. The author is sorry for how things turned out in the end and wish you find it in you to forgive them... 
+                miles away... the author is sorry for how things turned out in the end and wishes that you find it in you to forgive them... 
                 explore something special made
                 just for you! (because you are special 😊)
               </p>
